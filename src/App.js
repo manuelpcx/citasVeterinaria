1cd4ac0058507f33fede5f7fb7f8da8d22fb306a
jesus <jesus.medina05@gmail.com>
2020-05-07 01:35:15 -0400

    Creacion de app para crear citas en un veterinario, mas creacion de formulario y guardar los datos en el state principal

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Fragment, useState} from 'react';
+import Formulario from './components/Formulario'
 
 function App() {
+
+  // Arreglo de citas
+  const [citas, guardarCitas] = useState([]);
+
+  // Funcion que tome las citas actuales y agrege las nuevas
+  const crearCita = cita => {
+    guardarCitas([
+      ...citas,
+      cita
+    ]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <h1>Administrador de pacientes</h1>
+
+      <div className="container">
+        <div className="row">
+          <div className="one-half column">
+            <Formulario
+              crearCita={crearCita}
+            />
+          </div>
+          <div className="one-half column">
+            2
+          </div>
+        </div>
+      </div>
+    </Fragment>
   );
 }
 
